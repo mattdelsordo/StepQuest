cccac7cba4fd1a6f0a32a1f828baae82b5fe7dac
Matt DelSordo <mattdelsordo@gmail.com>
2017-04-16 21:52:41 -0400

    Started working on character creation screen.

---
+++ app/src/main/java/project3/csc214/stepquest/ui/CharacterCreationActivity.java
@@ -0,0 +1,15 @@
+package project3.csc214.stepquest.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import project3.csc214.stepquest.R;
+
+public class CharacterCreationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_creation);
+    }
+}
