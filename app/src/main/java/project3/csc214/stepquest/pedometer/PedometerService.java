06485e0b78b2fc157d3703a8a6a1d6acf2c371cb
mattdelsordo <mattdelsordo@gmail.com>
2017-05-03 23:03:08 -0400

    This is what I submitted and its kinda shitty but hey

--- app/src/main/java/project3/csc214/stepquest/pedometer/PedometerService.java
+++ app/src/main/java/project3/csc214/stepquest/pedometer/PedometerService.java
@@ -65,7 +65,7 @@ public class PedometerService extends Service implements SensorEventListener{
         //TODO: this delay may be overkill
         mManager.registerListener(this, mStepSensor, SensorManager.SENSOR_DELAY_FASTEST);
 
-        simulateSteps();
+       // simulateSteps();
 
 
         //set up save timer
