09248a176c5ff1837d80c77d7c6a5cb1a68218bf
Matt DelSordo <mattdelsordo@gmail.com>
2017-05-30 18:42:14 -0400

    Adjusted CR duration values

--- app/src/main/java/project3/csc214/stepquest/data/EventList.java
+++ app/src/main/java/project3/csc214/stepquest/data/EventList.java
@@ -35,7 +35,6 @@ public class EventList {
     private Random rand;
     private Context mAppContext;
 
-    //TODO: reconfigure how monsters are randomized
     private EventList(Context appContext){
         mAppContext = appContext.getApplicationContext();
         //mMonsters = new ArrayList<>();
