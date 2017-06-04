e7863af82282b88e82ddc656400f0318aa9c973e
Matt DelSordo <mattdelsordo@gmail.com>
2017-06-04 11:00:09 -0400

    This is the initial beta release

--- app/src/main/java/com/mdelsordo/stepquest/services/PedometerService.java
+++ app/src/main/java/com/mdelsordo/stepquest/services/PedometerService.java
@@ -66,6 +66,7 @@ public class PedometerService extends Service implements SensorEventListener, Ev
         //TODO: this delay may be overkill
         mManager.registerListener(this, mStepSensor, SensorManager.SENSOR_DELAY_FASTEST);
 
+        /** THIS MUST BE COMMENTED OUT IN PRODUCTION **/
         //simulateSteps();
 
 
